b5b52bcca1d42ad9ad6e64be704a442bcc3182fe
Edgss <eduardoogomess211@gmail.com>
2025-04-04 17:07:26 -0300

    portfolio mobile

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let trilho = document.getElementById("trilho")
+let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 
-trilho.addEventListener("click", ()=>{
+trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
 })
